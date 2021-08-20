c57e94fcf3628a968f335aa3cdc6e85524d98253
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-08-20 09:21:49 +0600

    big big big refactoring

--- js/script.js
+++ js/script.js
@@ -2,17 +2,10 @@
 const art = document.getElementById('art');
 const descField = document.getElementById('desc');
 const priceField = document.getElementById('price');
-const freeShippingBtn = document.getElementById('free-shipping-btn');
-const expressShippingBtn = document.getElementById('express-shipping-btn');
-const freeShippingChargeField = document.getElementById('free-shipping-charge');
-const expressShippingChargeField = document.getElementById(
-  'express-shipping-charge'
-);
+const shippingBtns = document.getElementsByClassName('shipping-btn');
+const tabBtns = document.getElementsByClassName('tab-btn');
 const shippingChargeField = document.getElementById('shipping-charge');
 const totalField = document.getElementById('total');
-const tab1Btn = document.getElementById('first');
-const tab2Btn = document.getElementById('second');
-const tab3Btn = document.getElementById('third');
 
 // price
 const freeShippingCharge = 0;
@@ -35,15 +28,28 @@ const tab3Contents = {
   desc: 'Third - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum beatae laboriosam, commodi quis tempore necessitatibus. Voluptatibus minima aut non doloremque?',
   price: 400,
 };
-let selectedTabs = {};
+let selectedTab = {};
 
 // functions
-function changeTabs(tab) {
+function changeTabs(tab, tabBtn) {
   art.src = tab.imageUrl;
   descField.innerText = tab.desc;
   priceField.innerText = tab.price;
-  updateBalance(tab.price, shippingCost);
-  selectedTabs = tab;
+  selectedTab = tab;
+  updateBalance(selectedTab.price, shippingCost);
+  selected(tabBtn);
+}
+
+function updateBalance(price, shippingCost) {
+  const total = price + shippingCost;
+  totalField.innerText = total;
+}
+
+function changeShippingMethod(selectedTabs, shippingMethod, shippingBtn) {
+  shippingCost = eval(shippingMethod + 'ShippingCharge');
+  shippingChargeField.innerText = shippingCost;
+  updateBalance(selectedTabs.price, shippingCost);
+  selected(shippingBtn);
 }
 
 function selected(item) {
@@ -55,42 +61,21 @@ function selected(item) {
   item.classList.add('selected');
 }
 
-function updateBalance(price, shippingCost) {
-  const total = price + shippingCost;
-  totalField.innerText = total;
-  shippingChargeField.innerText = shippingCost;
+// event listeners
+for (const tabBtn of tabBtns) {
+  tabBtn.addEventListener('click', function () {
+    const tabContent = tabBtn.dataset.tabcontent;
+    changeTabs(eval(tabContent), tabBtn);
+  });
 }
 
-// event listeners
-tab1Btn.addEventListener('click', function () {
-  changeTabs(tab1Contents);
-  selected(tab1Btn);
-});
-tab2Btn.addEventListener('click', function () {
-  changeTabs(tab2Contents);
-  selected(tab2Btn);
-});
-tab3Btn.addEventListener('click', function () {
-  changeTabs(tab3Contents);
-  selected(tab3Btn);
-});
-
-freeShippingBtn.addEventListener('click', function () {
-  shippingCost = freeShippingCharge;
-  selected(freeShippingBtn);
-  updateBalance(selectedTabs.price, shippingCost);
-});
-expressShippingBtn.addEventListener('click', function () {
-  shippingCost = expressShippingCharge;
-  selected(expressShippingBtn);
-  updateBalance(selectedTabs.price, shippingCost);
-});
+for (const shippingBtn of shippingBtns) {
+  shippingBtn.addEventListener('click', function () {
+    const shippingMethod = shippingBtn.dataset.method;
+    changeShippingMethod(selectedTab, shippingMethod, shippingBtn);
+  });
+}
 
 // initial
-changeTabs(tab1Contents);
-selectedTabs = tab1Contents;
-selected(tab1Btn);
-selected(freeShippingBtn);
-updateBalance(selectedTabs.price, shippingCost);
-freeShippingChargeField.innerText = freeShippingCharge;
-expressShippingChargeField.innerText = expressShippingCharge;
+changeTabs(tab1Contents, tabBtns[0]);
+changeShippingMethod(selectedTab, 'free', shippingBtns[0]);
